181e265bd98410f0ff3ee54321bd993cc457034f
fauzihazim <fauzihazim28@gmail.com>
2025-02-08 14:02:11 +0700

    first commit

---
+++ backend/index.ts
@@ -0,0 +1,10 @@
+import express, { Request, Response } from 'express';
+const app = express();
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello, TypeScript with Node.js!');
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000');
+});
