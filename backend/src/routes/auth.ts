5739aac47cbeb2fda5b9fffdd4a8e6d9c182c53f
fauzihazim <fauzihazim28@gmail.com>
2025-02-10 16:21:29 +0700

    fix: cookies fix signed

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -1,5 +1,8 @@
 import express from 'express';
+import cookieParser from 'cookie-parser';
+
 const app = express.Router();
+app.use(cookieParser('yourSecretKey'));
 
 import { login } from "../controllers/auth";
 
